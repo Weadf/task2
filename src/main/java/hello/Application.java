2628d95c0d4ab180def19325c56c4a2d1918928e
Weadf <SABELFELD.00@gmail.com>
2017-10-16 01:25:39 +0300

    Loads SQL scripts? schema.sql and data.sql

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,36 +1,19 @@
 package hello;
 
-import hello.dao.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import javax.sql.DataSource;
-
-
-import java.util.List;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-   // @Qualifier("dataSource")
-  // @Qualifier
-
-
+public class Application {
 
 
     @Autowired
-    private ContactRepository contactRepository;
+
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    public void run(String... strings) throws Exception {
-        System.out.println("Display all customers...");
-        List<Contacts> list = contactRepository.findAll();
-        list.forEach(x -> System.out.println(x.getName()));
-    }
+
 }
--- src/main/java/hello/ContactsController.java
+++ src/main/java/hello/ContactsController.java
@@ -1,10 +1,9 @@
 package hello;
 
 import hello.dao.ContactRepository;
+import jdk.nashorn.internal.runtime.regexp.RegExp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,29 +18,32 @@ public class ContactsController {
     private ContactRepository contactRepository;
 
     @RequestMapping(value = "/contacts")
+    public List<Contacts> contacts(@RequestParam(value = "nameFilter", defaultValue = "not") String name) {
+        List<Contacts> contacts;
+        if (name.equals("A")) {
+           contacts = sortContacts("^A.*$");
 
-    public List<Contacts> contacts (@RequestParam(value = "nameFilter", defaultValue = "not")String name) {
-        List<Contacts> contacts = new ArrayList() ;
-        System.out.println(name);
-if (name.equals("A")){
-    for (Contacts contact : contactRepository.findAll()){
-        if (!contact.getName().matches("^A.*$")){
-            contacts.add(contact);
+        } else if (name.equals("B")) {
+
+            contacts = sortContacts("^.*[aei].*$");
+        } else {
+
+
+            contacts = this.contactRepository.findAll();
         }
+        return contacts;
     }
 
-}else if (name.equals("B")){
-    for (Contacts contact : contactRepository.findAll()){
-        if (!contact.getName().matches("^.*[aei].*$")){
-            contacts.add(contact);
-        }
-    }
-}
-else {
 
-        contacts = this.contactRepository.findAll();
-}
-return contacts;
+    private List<Contacts> sortContacts( String regExp){
+        List<Contacts> contacts = new ArrayList();
+
+        for (Contacts contact : contactRepository.findAll()) {
+            if (!contact.getName().matches(regExp)) {
+                contacts.add(contact);
+            }
+        }
+        return contacts;
     }
 }
 
